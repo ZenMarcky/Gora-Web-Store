ddef550d84bbaa9424842b1f90b6d12e19e9fbfb
ZenMarcky <zenvlog38@gmail.com>
2022-12-02 14:18:16 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+//document.querySelector("#navOpen").addEventListener('click', openNav);
+//document.querySelector("#navClose").addEventListener('click', closeNav);
+
+function openNav(){
+    document.querySelector(".hideNavbar").style.marginRight = "0"
+}
+function closeNav(){
+
+
+  document.querySelector(".hideNavbar").style.marginRight = "-210px"
+
+}
